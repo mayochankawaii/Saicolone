110881bd81bbcc34134acfc604595d509217418d
EC2 Default User <ec2-user@ip-172-31-2-186.ap-northeast-1.compute.internal>
2024-08-06 09:55:38 +0000

    複数権限追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  
-  def after_sign_in_path_for(resource)
-    public_mypage_path
-  end
+  before_action :configure_sign_in_params, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -28,4 +25,8 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+   end
 end
