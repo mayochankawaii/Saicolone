af58326e947172ee85af525aa354cec4c7abb64d
mayochankawaii <someda111523@gmail.com>
2024-08-22 05:09:18 +0000

    管理者機能追加

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,4 +1,30 @@
 class Admin::GroupsController < ApplicationController
+
   def index
+    @group = Group.new
+    @groups = Group.all
+    @group_lists = Group.all.order(:id)
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @messages = @group.messages
+    @message = Message.new
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    redirect_to admin_groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
