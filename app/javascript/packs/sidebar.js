effa5b232b0cbed4659e53fcb1153f8e15f1e9b6
mayochankawaii <someda111523@gmail.com>
2024-08-25 01:25:44 +0000

    レイアウト調整

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -1,17 +1,23 @@
-var toggleBtn = document.querySelector('.sidebar-toggle');
-var sidebar = document.querySelector('.sidebar');
+// var toggleBtn = document.querySelector('.sidebar-toggle');
+// var sidebar = document.querySelector('.sidebar');
 
-toggleBtn.addEventListener('click', function() {
-  toggleBtn.classList.toggle('is-closed');
-  sidebar.classList.toggle('is-closed');
-})
+// toggleBtn.addEventListener('click', function() {
+//   toggleBtn.classList.toggle('is-closed');
+//   sidebar.classList.toggle('is-closed');
+// })
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   var toggleBtn = document.querySelector('.sidebar-toggle');
-//   var sidebar = document.querySelector('.sidebar');
+document.addEventListener('DOMContentLoaded', function() {
+  var toggleBtn = document.querySelector('.sidebar-toggle');
+  var sidebar = document.querySelector('.sidebar');
 
-//   toggleBtn.addEventListener('click', function() {
-//     toggleBtn.classList.toggle('is-closed');
-//     sidebar.classList.toggle('is-closed');
-//   });
+  toggleBtn.addEventListener('click', function() {
+    toggleBtn.classList.toggle('is-closed');
+    sidebar.classList.toggle('is-closed');
+  });
+});
+
+/*global $*/
+// $(".sidebar-toggle").click(function () {
+//     $(this).toggleClass('active');
+//     $(".sidebar").fadeOut();
 // });
