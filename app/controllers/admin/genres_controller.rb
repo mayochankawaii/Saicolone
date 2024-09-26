9de282d3180de36ad8656b9b41c605e7e5964986
mayochankawaii <someda111523@gmail.com>
2024-09-26 07:31:17 +0000

    レイアウト2

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,10 +5,20 @@ class Admin::GenresController < ApplicationController
     @genres = Genre.all
   end
 
+  # def create
+  #   @genre = Genre.new(genre_params)
+  #   @genre.save
+  #   redirect_to admin_genres_path
+  # end
   def create
     @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to admin_genres_path
+
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      redirect_to admin_genres_path, alert: '空白、または登録済みのジャンルは保存できません'
+    end
   end
 
   def edit
@@ -16,9 +26,13 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
-    genre = Genre.find(params[:id])
-    genre.update(genre_params)
-    redirect_to admin_genres_path
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      redirect_to edit_admin_genre_path(@genre), alert: '空白、または登録済みのジャンルは保存できません'
+    end
   end
 
   private
