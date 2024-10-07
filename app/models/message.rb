c7b7bfa9c6b00fcef734d5ef76f793e3d45a461d
mayochankawaii <someda111523@gmail.com>
2024-10-07 09:39:11 +0000

    通知機能2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   validates :content, presence: true, length: { maximum: 300 }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,6 @@ class Room < ApplicationRecord
   has_many :direct_messages, dependent: :destroy
   has_many :entries,         dependent: :destroy
   has_many :users,           through: :entries
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
 end
